9186fd25927bfa8e3853fbcb42e29ea60b384447
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-11-26 04:40:49 +0800

    avafdvfdav

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,34 @@ import '@nomiclabs/hardhat-etherscan';
 import 'solidity-coverage';
 import 'hardhat-gas-reporter';
 
+const GOERLI_IPS_URL = process.env.GOERLI_IPS_URL || 'https://eth-rinkeby';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key';
+const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY || 'key';
+
 const config: HardhatUserConfig = {
-    solidity: '0.8.17'
+    solidity: '0.8.17',
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    },
+    networks: {
+        goerli: {
+            url: GOERLI_IPS_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5
+        },
+        localhost: {
+            url: 'http://127.0.0.1:8545/',
+            chainId: 31337
+        }
+    },
+    gasReporter: {
+        enabled: true, //if need report switch to true
+        outputFile: 'Reports/gas-report.txt',
+        noColors: true,
+        currency: 'USD',
+        coinmarketcap: COIN_MARKET_CAP_API_KEY
+    }
 };
 
 export default config;
--- test/Lock.ts
+++ test/Lock.ts
@@ -1,31 +1,35 @@
-import { SimpleStorage__factory } from './../typechain-types/factories/SimpleStorage__factory'
-import { assert } from 'chai'
+import { SimpleStorage__factory } from '../typechain-types/factories/SimpleStorage__factory'
+import { assert, expect } from 'chai'
 import { ethers } from 'hardhat'
 import { SimpleStorage } from '../typechain-types/SimpleStorage'
 
 describe('SimpleStorage', () => {
-  let simpleStorage: SimpleStorage, SimpleStorageFactory: SimpleStorage__factory
+  let simpleStorage: SimpleStorage
+  let SimpleStorageFactory: SimpleStorage__factory
   beforeEach(async () => {
-    SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage')
+    SimpleStorageFactory = (await ethers.getContractFactory(
+      'SimpleStorage',
+    )) as SimpleStorage__factory
     simpleStorage = await SimpleStorageFactory.deploy()
   })
-  it('Should Start with Favorite Number: 0', async () => {
-    const currentValue = await simpleStorage.retrieve()
-    assert(String(currentValue), '0')
+  it('Should start with a favorite number of 0', async function () {
+    let currentValue = await simpleStorage.retrieve()
+    expect(currentValue).to.equal(0)
   })
-  // it.only('Should Update to value when we call Store: 7', async () => {
-  it('Should Update to value when we call Store: 7', async () => {
-    const value = '7'
-    await simpleStorage.store(value)
-    const currentValue = await simpleStorage.retrieve()
-    assert(String(currentValue), value)
+  it('Should update when we call store', async function () {
+    let expectedValue = 7
+    let transactionResponse = await simpleStorage.store(expectedValue)
+    let transactionReceipt = await transactionResponse.wait()
+    let currentValue = await simpleStorage.retrieve()
+    expect(currentValue).to.equal(expectedValue)
   })
   it('Adding People with addPerson function', async () => {
     const name = 'Joe'
     const favoriteNumber = '1963'
     await simpleStorage.addPerson(name, favoriteNumber)
     const usersNumber = await simpleStorage.people(0)
-    assert(favoriteNumber, usersNumber.favoriteNumber)
+    console.log(usersNumber.favoriteNumber)
+    assert(favoriteNumber, String(usersNumber.favoriteNumber))
     assert(name, usersNumber.name)
   })
 })
